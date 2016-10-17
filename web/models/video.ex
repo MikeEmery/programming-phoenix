852b90d7dfc6afbb6cdad634216a674e8fbc6a9f
Mike Emery <memery@yahoo-inc.com>
2016-10-16 17:02:06 -0700

    something that resembles websockets

--- web/models/video.ex
+++ web/models/video.ex
@@ -2,12 +2,15 @@ defmodule Rumbl.Video do
   use Rumbl.Web, :model
 
   @required_params [:url, :title, :description]
-  @optional_params [:category_id]
+  @optional_params [:category_id, :slug]
+
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
 
   schema "videos" do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
 
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
@@ -21,7 +24,28 @@ defmodule Rumbl.Video do
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, @required_params, @optional_params)
+    |> slugify_title
     |> validate_required(@required_params)
     |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  def slugify(str) do
+    str
+      |> String.downcase
+      |> String.replace(~r/[^\w-]+/u, "-")
+  end
+end
+
+defimpl Phoenix.Param, for: Rumbl.Video do
+  def to_param(%{slug: slug, id: id}) do
+    "#{id}-#{slug}"
   end
 end
